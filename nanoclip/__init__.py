0c07915beda1504e4569be49688950696a017a7c
Tom Pollak <tompollak1000@gmail.com>
2024-10-21 09:18:57 +0100

    done tutorial

--- nanoclip/__init__.py
+++ nanoclip/__init__.py
@@ -1,1 +1,2 @@
-from nanoclip.weights.clip import clip_vit_b_16 # noqa: F401
+from nanoclip.vit import ViT, ViTConfig, build_preprocessor  # noqa: F401
+from nanoclip.weights.clip import clip_vit_b_16  # noqa: F401
